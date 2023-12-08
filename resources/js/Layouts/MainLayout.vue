08e4bbccbabfaf56a0f5d7107dea49fea8b281e6
Liviu Hariton <liviu.hariton@gmail.com>
2023-12-08 21:39:30 +0200

    Reactive data in Vue with ref()

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,6 +2,13 @@
     <Link href="/">Show page</Link> |
     <Link href="/hello">Show page</Link>
 
+    <div>
+        <!-- reactivity -->
+        <h3>The page with time {{ timer }}</h3>
+
+        <!-- <button @click="timer++">Timer: {{ timer }}</button> -->
+    </div>
+
     <!--
     In here it will be rendered the content from the components
     that are using this layout via <MainLayout>...</MainLayout> or <main-layout>...</main-layout>
@@ -11,4 +18,13 @@
 
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3';
+
+// reactivity
+import {ref} from "vue";
+
+const timer = ref(0);
+// increment the timer every second with 1
+setInterval(() => {
+    timer.value++;
+}, 1000);
 </script>
