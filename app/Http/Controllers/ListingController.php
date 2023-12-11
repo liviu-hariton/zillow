482bc9df624a9707979b831b410eb9c37c990200
Liviu Hariton <liviu.hariton@gmail.com>
2023-12-11 23:48:04 +0200

    Create forms and submit them

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -22,7 +22,7 @@ class ListingController extends Controller
      */
     public function create()
     {
-        //
+        return inertia('Listing/Create');
     }
 
     /**
@@ -30,7 +30,7 @@ class ListingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request->all());
     }
 
     /**
