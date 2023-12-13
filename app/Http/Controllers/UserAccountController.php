446bf2a3538eac7337fff8ce3239a009ebe9b120
Liviu Hariton <liviu.hariton@gmail.com>
2023-12-13 20:33:54 +0200

    Create a new user account

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserAccountController extends Controller
 {
     public function create()
     {
         return inertia('UserAccount/Create');
+    }
+
+    public function store(Request $request)
+    {
+        // Validate the request and initiate the user model
+        $user = User::make(
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:8|confirmed',
+            ])
+        );
+
+        // Hash the password
+        $user->password = Hash::make($request->password);
+
+        // Save the user into the database
+        $user->save();
+
+        // Log the user in
+        Auth::login($user);
+
+        // Redirect the user back to the listing page with a success message
+        return redirect()->route('listing.index')->with('success', 'User created successfully.');
     }
 }
