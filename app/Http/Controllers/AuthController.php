493d885cac914a4298fb048e999d4e20449453b8
Liviu Hariton <liviu.hariton@gmail.com>
2023-12-13 19:41:02 +0200

    Login logic

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    // Display sign-in form
+    public function create()
+    {
+        return inertia('Auth/Login');
+    }
+
+    // Process sign-in form
+    public function store(Request $request)
+    {
+        if(!Auth::attempt(
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]), true
+        )) {
+            throw ValidationException::withMessages([
+                'email' => 'The provided credentials are incorrect.',
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect()->intended();
+    }
+
+    // Process sign-out
+    public function destroy()
+    {
+
+    }
+}
