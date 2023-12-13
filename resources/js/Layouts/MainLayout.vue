318014cf2ba721d16f11d5e49f122c77093eeb2f
Liviu Hariton <liviu.hariton@gmail.com>
2023-12-13 20:22:57 +0200

    User registration form

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -15,7 +15,8 @@
             <Link :href="route('logout')" method="delete" as="button">Sign out</Link>
           </div>
         </div>
-        <div v-else>
+        <div v-else class="flex items-center gap-2">
+          <Link :href="route('user-account.create')">Register</Link>
           <Link :href="route('login')">Sign-In</Link>
         </div>
       </nav>
