7ada5ced902d4b4082fc982b9593cac22b9b2282
Liviu Hariton <liviu.hariton@gmail.com>
2023-12-14 18:32:24 +0200

    Added pagination

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -24,7 +24,7 @@ class ListingController extends Controller
     public function index()
     {
         return inertia('Listing/Index', [
-            'listings' => Listing::all(),
+            'listings' => Listing::orderByDesc('created_at')->paginate(10),
         ]);
     }
 
