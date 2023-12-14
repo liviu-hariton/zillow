149ee00d15ea3b8645df697031e5b04a5d052bc1
Liviu Hariton <liviu.hariton@gmail.com>
2023-12-14 21:45:46 +0200

    Vue watchers

--- resources/js/Pages/Realtor/Index.vue
+++ resources/js/Pages/Realtor/Index.vue
@@ -1,6 +1,10 @@
 <template>
   <h1 class="text-3xl mb-4">Your Listings</h1>
-  <section class="mb-8">Filters</section>
+
+  <section>
+    <RealtorFilters />
+  </section>
+
   <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
     <Box v-for="listing in listings" :key="listing.id">
       <div class="flex flex-col md:flex-row gap-2 md:items-center justify-between">
@@ -28,6 +32,7 @@ import ListingSpace from '@/Components/ListingSpace.vue'
 import Price from '@/Components/Price.vue'
 import Box from '@/Components/UI/Box.vue'
 import { Link } from '@inertiajs/inertia-vue3'
+import RealtorFilters from '@/Components/RealtorFilters.vue'
 
 defineProps({
   listings: {
