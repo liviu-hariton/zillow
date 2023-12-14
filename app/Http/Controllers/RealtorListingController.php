634c9bc2b7953ad4ea58ac6c439dbcbb965365af
Liviu Hariton <liviu.hariton@gmail.com>
2023-12-14 20:48:35 +0200

    My account section

---
+++ app/Http/Controllers/RealtorListingController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RealtorListingController extends Controller
+{
+    public function index()
+    {
+        return inertia('Realtor/Index', [
+            'listings' => auth()->user()->listings,
+        ]);
+    }
+}
